e17be921e91ebf2a125ae552f0e6bff9d47e090e
savadkoohy <somaye.savadkoohy222@gmail.com>
2023-10-12 18:06:54 +0330

    part #7

--- dist/js/user.js
+++ dist/js/user.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const addUserButton = document.getElementById('add-user-button')
     addUserButton.addEventListener('click',()=>{
         document.querySelector('.add_user_modal').classList.add('show')
-        document.querySelector('.add_user_modal_back').classList.remove('dic-none')
+        document.querySelector('.add_user_modal_back').classList.remove('dis-none')
     })
 
-    document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+    document.querySelector('.add_user_modal_back').addEventListener('click',(e)=>{
         document.querySelector('.add_user_modal').classList.remove('show')
-        e.target.classList.add('dis-none')
+        e.target.add('dis-none')
     })
 
   });
