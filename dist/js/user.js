8344a6cf198a643daa3fe2b823ae998ddb8eeec7
savadkoohy <somaye.savadkoohy222@gmail.com>
2023-10-12 14:47:48 +0330

    part #6

---
+++ dist/js/user.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
+
+
+    const addUserButton = document.getElementById('add-user-button')
+    addUserButton.addEventListener('click',()=>{
+        document.querySelector('.add_user_modal').classList.add('show')
+        document.querySelector('.add_user_modal_back').classList.remove('dic-none')
+    })
+
+    document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+        document.querySelector('.add_user_modal').classList.remove('show')
+        e.target.classList.add('dis-none')
+    })
+
+  });
