cd39bf0186beae728d1da63889f054aec8c00ec3
savadkoohy <somaye.savadkoohy222@gmail.com>
2023-10-15 10:07:40 +0330

    part #12

--- dist/js/alpinejsControlles/usersControlles.js
+++ dist/js/alpinejsControlles/usersControlles.js
@@ -2,18 +2,51 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData',function(){
         return{
             users:[],
+            pageUsers:[],
             isLoading:false,
             showAddModal:false,
+            pageCount:1,
+            itemCount:4,
+            currentPage:1,
             getUsers(){
                 this.isLoading=true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                 this.users=res.data 
-                console.log(res);
+                this.pagination()
                 }).catch(error=>{
                     console.log(error.message);
                 }).finally(()=>{
                     this.isLoading=false;
                 })
+            },
+            pagination(){
+                this.pageCount = Math.ceil(this.users.length / this.itemCount)
+                let  start = (this.currentPage * this.itemCount) - this.itemCount
+                let end = this.currentPage * this.itemCount
+                this.pageUsers = this.users.slice(start,end)
+            },
+
+            nextpage(){
+                this.currentPage++
+                if(this.currentPage >this.pageCount){
+                    this.currentPage =this.pageCount
+                }
+                this.pagination()
+            },
+            
+            prevpage(){
+                this.currentPage--
+                if(this.currentPage <1){
+                    this.currentPage =1
+                }
+                this.pagination()
+            },
+            handelChangeItemsCount(){
+                this.itemCount = e.value
+                if(this.itemCount <1)this.itemCount =1
+                if(this.itemCount>this.users.length)this.itemCount = this.users.length
+                this.pagination()
+                console.log(e.value);
             }
         }
     })
